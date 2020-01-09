f04cdd810fcccae10ae485c51f7d345988d3b1f4
Alexander Lang <alex@upstre.am>
2020-01-09 15:43:20 +0100

    make task/container optional

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,20 +4,22 @@ set -e o pipefail
 
 # Validate input
 [ -z "$INPUT_CLUSTER" ] && (echo "Missing Cluster Name" && exit 1)
-[ -z "$INPUT_CONTAINER" ] && (echo "Missing Container Name" && exit 1)
-[ -z "$INPUT_TASK" ] && (echo "Missing Task Name" && exit 1)
 [ -z "$INPUT_SERVICE" ] && (echo "Missing Service Name" && exit 1)
 
 TIMEOUT=${INPUT_TIMEOUT:-300}
+command=$(printf "ecs deploy $INPUT_CLUSTER $INPUT_SERVICE --timeout $TIMEOUT")
+
+if [ ! -z "$INPUT_TASK" ];
+then
+    command=$(printf "$command --task $INPUT_TASK")
+fi
 
 if [ ! -z "$INPUT_ENVFILE" ];
 then
-    [ ! -f $INPUT_ENVFILE ] && echo "$INPUT_ENVFILE not found" && exit 1 
+    [ ! -f $INPUT_ENVFILE ] && echo "$INPUT_ENVFILE not found" && exit 1
     env_variables=$(grep -v '^#' "$INPUT_ENVFILE" | grep . | sed -E 's/=/ /1' | awk -v CONTAINER="$INPUT_CONTAINER" '{print "-e " CONTAINER " " $1 " " $2}')
     # make string command to be evaluated (one line string)
-    command=$(printf "ecs deploy $INPUT_CLUSTER $INPUT_SERVICE --timeout $TIMEOUT --task $INPUT_TASK $env_variables --exclusive-env")
-else
-    command=$(printf "ecs deploy $INPUT_CLUSTER $INPUT_SERVICE --timeout $TIMEOUT --task $INPUT_TASK")
+    command=$(printf "$command $env_variables --exclusive-env")
 fi
 
 # fire command
